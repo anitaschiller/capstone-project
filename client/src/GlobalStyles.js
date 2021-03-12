277d142e9df64ce729bc6e86953eaf21eac92e57
Anita Schiller <anita.s1@gmx.de>
2021-03-12 10:55:52 +0100

    Created basic setup

---
+++ client/src/GlobalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default CreateGlobalStyle`
+
+:root {
+  --primary: #07D6A0;
+  --secondary: #354AA8;
+  --background: #F2F0F0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+`;
